3eb97c3e3e9c8e9a2d872d9b47972f25dcbc7dd4
abedcsj <abedcsj@gmail.com>
2024-09-14 15:47:36 +0900

    mapping 종류

--- week3/src/main/java/com/server/pard/week3/RequestParamController.java
+++ week3/src/main/java/com/server/pard/week3/RequestParamController.java
@@ -4,6 +4,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+
 @RestController
 public class RequestParamController {
     @RequestMapping("/v1")
@@ -19,5 +21,22 @@ public class RequestParamController {
     public String param3(String name,int age){
         return "Parameter name 3번: "+name +" 나이: "+age;
     }
+    @RequestMapping("/v4")
+    public String param4(@RequestParam String name,@RequestParam(required=false) Integer age){
+        if(age==null){
+            return "4번 request param " +name+" age: 입력칸 없음";
+        }
+        return "4 request param: "+name+" age: "+age;
+    }
+    @RequestMapping("/v5")
+    public String param5(@RequestParam(required = true,defaultValue="파드") String name,@RequestParam(required=false,defaultValue = "-1") Integer age){
+        return "5 request param: "+name+" age: "+age;
+    }
+    @RequestMapping("/v6")
+    public String param6(@RequestParam Map<String,Object> map){
+        return "6 request param: "+map.get("name")+map.get("age");
+    }
+
+
 
 }
---
+++ week3/src/main/java/com/server/pard/week3/RestMethodController.java
@@ -0,0 +1,34 @@
+package com.server.pard.week3;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/user")
+public class RestMethodController {
+    @GetMapping("")
+    public String getUsers(){
+        return"Get Method controller";
+    }
+
+    @PostMapping("")
+    public String postUsers(){
+        return"Post Method controller";
+    }
+
+    @GetMapping("/{userId}")
+    public String getUserById(@PathVariable Long userId){
+        return "userId : "+userId;
+    }
+
+    @PatchMapping("/{userId}")
+    public String patchUserById(@PathVariable Long userId){
+        return userId+"번 유저의 정보를 수정";
+    }
+
+    @DeleteMapping("/{userId}")
+    public String deleteUser(@PathVariable Long userId){
+        return userId+"번 유저의 정보 삭제";
+    }
+
+}
+
