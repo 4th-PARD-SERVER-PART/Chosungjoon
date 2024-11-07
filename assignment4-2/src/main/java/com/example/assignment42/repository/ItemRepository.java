3a189e1253f3968641a5eb560ddda6a8d06d0243
abedcsj <abedcsj@gmail.com>
2024-11-07 15:50:18 +0900

    Delete assignment4-2 directory

--- assignment4-2/src/main/java/com/example/assignment42/repository/ItemRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.assignment42.repository;
-import com.example.assignment42.model.Item;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ItemRepository extends JpaRepository<Item, Long> {
-    // 필요한 경우, 사용자 정의 쿼리를 여기에 추가할 수 있습니다.
-}
